a4dfbc16b7223e0260f4de72cc63ee0569f303df
Alejandro Lagos <alejandrolagos.dev@gmail.com>
2023-07-24 21:11:40 -0500

    basic particles

--- src/particles.js
+++ src/particles.js
@@ -24,11 +24,44 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const particlesGeometry1 = new THREE.SphereGeometry(1, 32, 32)
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.2,
+    sizeAttenuation: true
+})
+
+
+const particles = new THREE.Points(particlesGeometry1, particlesMaterial)
+//scene.add(particles)
+
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 500
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const particles2 = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles2)
+
+//particlesMaterial.color = new THREE.Color('#ffc')
+
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+particlesMaterial.vertexColors = true
+
+const particleTexture = textureLoader.load('/3.png')
+
+// particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
 
 /**
  * Sizes
@@ -68,6 +101,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    for(let i = 0; i < count; i++)
+    {
+        let i3 = i * 3
+
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
